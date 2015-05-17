35c4e18121e5e7de1a6461a4508c0c06da0864cc
egjimenezg <egjimenezg@gmail.com>
2015-05-17 10:35:54 -0500

    coding #4 adding bit shift operations

--- RegisterFile/RegisterFile.vhd
+++ RegisterFile/RegisterFile.vhd
@@ -1,6 +1,8 @@
 -- Author: Gamaliel Jiménez
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity RegisterFile is Port(
   read_register1,read_register2 : in std_logic_vector(3 downto 0);   
@@ -18,8 +20,20 @@ architecture Behavioral of RegisterFile is
 begin
   process(clock,clear)
     begin
-
-  
+      if(clear='1') then
+        for i in 0 to 15 loop
+          regs(i) <= X"0000";
+        end loop;
+      elsif(clock'event and clock='1') then
+        if(wr='1' and she='0') then
+          regs(conv_integer(write_register)) <= write_data;
+      elsif(wr='0' and she='1') then
+        if(dir='1') then
+          regs(conv_integer(write_register)) <= to_stdlogicvector(to_bitvector(regs(conv_integer(read_register1))) sll conv_integer(shamt));
+        else
+          regs(conv_integer(write_register)) <= to_stdlogicvector(to_bitvector(regs(conv_integer(read_register1))) srl conv_integer(shamt)); 
+        end if;
+      end if;
+    end if;
   end process;
 end Behavioral;
-
