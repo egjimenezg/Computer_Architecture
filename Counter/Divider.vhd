003585bea6a4ddbf51bd8c406684f1c2e1094f13
egjimenezg <egjimenezg@gmail.com>
2015-07-04 16:59:53 -0500

    coding #7 updating divider

--- Counter/Divider.vhd
+++ Counter/Divider.vhd
@@ -1,24 +1,27 @@
--- Author: Gamaliel JimÃ©nez
+-- Author: Gamaliel Jiménez
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity Divider is Port(
   clock_oscilator,clear : in std_logic;
-  clock : out std_logic
+  clock : inout std_logic
 );
 end Divider;
 
 architecture Behavioral of Divider is
-  signal counter : std_logic_vector(25 downto 0);
+  signal counter : integer range 0 to 25000000;
 begin
   process(clock_oscilator,clear)
   begin
     if(clear='1') then
-      counter <= (others => '0');
-    elsif(rising_edge(clock_oscilator)) then
+      counter <= 0;
+      clock <= '0';
+    elsif(RISING_EDGE(clock_oscilator)) then
       counter <= counter+1;
-      clock <= counter(0);
+      if(counter = 0) then
+        clock <= not clock;
+      end if;
     end if;
   end process;
 end Behavioral;
