2c66419a007e217717fa8fb60c9fc21aa72e0d8d
egjimenezg <egjimenezg@gmail.com>
2015-07-08 06:52:51 -0500

    coding #9 adding some microinstructions

--- ControlUnity/FunctionCodeMemory.vhd
+++ ControlUnity/FunctionCodeMemory.vhd
@@ -11,7 +11,30 @@ entity FunctionCodeMemory is
 end FunctionCodeMemory;
 
 architecture Behavioral of FunctionCodeMemory is
-  type instruction is array (0 to (2**CODE_SIZE)-1) of std_logic_vector (INSTRUCTION_SIZE-1 downto 0);
+  type rom is array (0 to (2**CODE_SIZE)-1) of std_logic_vector (INSTRUCTION_SIZE-1 downto 0);
+  --TODO: Convert microinstructions to Hex format.
+  constant memory : rom := ("00000000010000000000", --LI
+                            "00000100010000001000", --LWI
+                            "00000110010100110000", --LW
+                            "00001000000000001100", --SWI
+                            "00001010000100110010", --SW
+                            "00000100010000110011", --ADD
+                            "00000100010001110011", --SUB
+                            "00000100010100110011", --ADDI
+                            "00000100010101110011", --SUBI									 
+                            "00000100010000000011", --AND
+                            "00000100010000010011", --OR
+                            "00000100010000100011", --XOR
+                            "00000100010011010011", --NAND
+                            "00000100010011000011", --NOR
+                            "00000100010001100011", --XNOR
+                            "00000100010011010011", --NOT									 
+                            "00000110010100000011", --ANDI
+                            "00000110010100010011", --ORI
+                            "00000110010100100011", --XORI
+                            "00000110010111010011", --NANDI
+                            "00000110010111000011", --NORI
+                            "00000110010110100011"); --XNORI
 begin
 
 end Behavioral;
